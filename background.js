8187f51769f7a9ab48b444786f2d790c71f0aa68
wudi1940 <wudi1940@qq.com>
2024-04-10 10:32:17 +0800

    Compatible for single quotation mark.

--- background.js
+++ background.js
@@ -5,10 +5,15 @@ chrome.commands.onCommand.addListener(function (command) {
             console.log(subject)
 
             const subjectPattern = /: (.)(.*?)\1/;
+            const subjectPattern2 = /:\s‘(.*?)’/;
             const subjectMatch = subjectPattern.exec(subject);
+            const subjectMatch2 = subjectPattern2.exec(subject);
 
             if (subjectMatch && subjectMatch[2]) {
                 const textToCopy = subjectMatch[2];
+                copyToClipboard(textToCopy);
+            } else if (subjectMatch2 && subjectMatch2[1]) {
+                const textToCopy = subjectMatch2[1];
                 copyToClipboard(textToCopy);
             }
         });
