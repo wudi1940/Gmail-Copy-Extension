db1044d5d4c0b3a040da0f5be895700e934791cf
wudi1940 <wudi1940@qq.com>
2025-07-24 16:40:43 +0800

    Update to Manifest V3.

--- background.js
+++ background.js
@@ -1,22 +1,33 @@
-chrome.commands.onCommand.addListener(function (command) {
+chrome.commands.onCommand.addListener(async (command) => {
     if (command === "copy-gmail-subject") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            const subject = tabs[0].title
-            console.log(subject)
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        const subject = tab.title;
+        console.log(subject);
 
-            const subjectPattern = /: (.)(.*?)\1/;
-            const subjectPattern2 = /:\s‘(.*?)’/;
-            const subjectMatch = subjectPattern.exec(subject);
-            const subjectMatch2 = subjectPattern2.exec(subject);
+        const subjectPattern = /: (.)(.*?)\1/;
+        const subjectPattern2 = /:\s'(.*?)'/;
+        const subjectMatch = subjectPattern.exec(subject);
+        const subjectMatch2 = subjectPattern2.exec(subject);
 
-            if (subjectMatch && subjectMatch[2]) {
-                const textToCopy = subjectMatch[2];
-                copyToClipboard(textToCopy);
-            } else if (subjectMatch2 && subjectMatch2[1]) {
-                const textToCopy = subjectMatch2[1];
-                copyToClipboard(textToCopy);
+        let textToCopy = null;
+        if (subjectMatch && subjectMatch[2]) {
+            textToCopy = subjectMatch[2];
+        } else if (subjectMatch2 && subjectMatch2[1]) {
+            textToCopy = subjectMatch2[1];
+        }
+
+        if (textToCopy) {
+            try {
+                await chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    func: copyToClipboard,
+                    args: [textToCopy]
+                });
+                console.log('Text copied to clipboard:', textToCopy);
+            } catch (err) {
+                console.error('Failed to copy text:', err);
             }
-        });
+        }
     }
 });
 
